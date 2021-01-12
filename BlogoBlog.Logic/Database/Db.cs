c98795d2a71a8340a9eeff5fbfac72ffbfa7c4da
Stanisław Gilewicz <stanislaw.gilewicz@efitness.com>
2021-01-12 22:56:50 +0100

    Add dry ioc

---
+++ BlogoBlog.Logic/Database/Db.cs
@@ -0,0 +1,16 @@
+﻿using BlogoBlog.Logic.Registration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DryIoc;
+
+namespace BlogoBlog.Database
+{
+    public static class Db
+    {
+        public static BlogoblogEntieties Context => IoC.Container.Resolve<BlogoblogEntieties>();
+        public static void Save() => Context.SaveChanges();
+    }
+}
---
+++ BlogoBlog.Logic/IoC/IoC.cs
@@ -0,0 +1,32 @@
+﻿using BlogoBlog.Database;
+using DryIoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogoBlog.Logic.Registration
+{
+    public static class IoC
+    {
+        private static IContainer _container;
+        public static IContainer Container
+        {
+            get
+            {
+                if (_container == null)
+                {
+                    _container = new Container(rules => rules.WithoutThrowOnRegisteringDisposableTransient()); 
+                    //.WithMvc(controllerReuse: Reuse.Transient,
+               //         throwIfUnresolved: t => t.IsController());
+                }
+                return _container;
+            }
+        }
+        public static void Initialize()
+        {
+            Container.Register<BlogoblogEntieties>(Reuse.Singleton);
+        }
+
+    }
+}
